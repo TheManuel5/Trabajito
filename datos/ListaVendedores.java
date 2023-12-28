36fafdb0061e063aeae1ed3a6cb1a242529ecb90
Prolexis <155090634+Prolexis@users.noreply.github.com>
2023-12-28 18:23:43 -0300

    Create ListaVendedores.java

---
+++ datos/ListaVendedores.java
@@ -0,0 +1,55 @@
+package datos;
+import java.util.*;
+import entidades.*;
+/**
+ *
+ * @author cristhian A.
+ */
+public class ListaVendedores {
+    private  static ArrayList<Vendedor>listaVendedores;
+    private  static Iterator<Vendedor> iterador;
+
+    public ListaVendedores() {
+        listaVendedores=new ArrayList<>();
+    }
+    public void setVendedor(Vendedor vendedor){
+        if(vendedor!=null)
+            listaVendedores.add(vendedor);
+    }
+    public Vendedor getVendedor(int pos){
+        if(pos>=0 &&pos<listaVendedores.size())
+            return listaVendedores.get(pos);
+        return null;
+    }
+    
+    @Override
+    public String toString(){
+        return listaVendedores.toString();
+    }
+    
+    
+    public  static int buscarVendedorPorDni (String numeroDni){
+          int i=0;
+        if(!listaVendedores.isEmpty()){
+         iterador=listaVendedores.iterator();
+           while(iterador.hasNext()){
+               if(iterador.next().getNumeroDni().equals(numeroDni))
+               return i;
+               else
+               i++;
+           }
+        }
+        return -1; 
+     } 
+    
+    public int buscarVendedorPorDni(Vendedor vendedor){
+    return listaVendedores.indexOf(vendedor);
+ }
+
+    
+    public static void listaOrdenadaPorApellido() {
+    if(listaVendedores.size() > 1) 
+    Collections.sort(listaVendedores,(Vendedor p1,Vendedor p2)->p1.getApellidos().compareTo(p2.getApellidos()));
+    }
+    
+}
