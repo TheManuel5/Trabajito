3a1f405b5e8b7c13b29ff296dac145e45a6779b3
DahGhoul <64115222+DahGhoul@users.noreply.github.com>
2023-12-28 23:00:17 -0500

    Update AgenciaConcesionario.java

--- Entidades/AgenciaConcesionario.java
+++ Entidades/AgenciaConcesionario.java
@@ -83,7 +83,7 @@ public class AgenciaConcesionario{
     @Override
     public String toString() {
         return "Concesionario " + nombre 
-               +"\nGerente : " + gerente + "\nVendedores : " + listaVendedores.toString() 
+               +"\nGerente : " + gerente.toString() + "\nVendedores : " + listaVendedores.toString() 
                + "\nAutomoviles : " + listaAutomoviles.toString() + "\nVentas : " + listaVentas.listarContenido()
                + "\nClientes : " + listaClientes.listarContenido() + '}';
     }
